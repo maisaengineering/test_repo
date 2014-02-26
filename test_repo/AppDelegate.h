64c598cc88681f3c10098b52665fa6d9e63fa5d7
maisaengineering <contact@maisasolutions.com>
2014-02-26 15:20:41 +0530

    initial commit

---
+++ test_repo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test_repo
+//
+//  Created by Maisa Solutions Pvt Ltd on 26/02/14.
+//  Copyright (c) 2014 Maisa Solutions Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test_repo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test_repo
+//
+//  Created by Maisa Solutions Pvt Ltd on 26/02/14.
+//  Copyright (c) 2014 Maisa Solutions Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
